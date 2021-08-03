1f9919d7538019ac5d3da16b4974dd9c4a1f1882
Niall Bermingham <87666591+niallcsb@users.noreply.github.com>
2021-08-03 13:37:57 -0400

    Add files via upload

--- script/data.js
+++ script/data.js
@@ -2,40 +2,105 @@ import Review_Link from "./Review_Link.js"
 
 // headline, subhead, link, image, date
 
-// Create review array
+// Create arrays
 
-const firstReview = new Review_Link(
-	"Grand Electric",
-	"This Parkdale based taco joint covers all the bases, great tacos, great margs, great atmosphere",
-	"#",
-	"../images/grandelectric.jpeg",
-	"July 28, 2021"
-)
+// First is the archive array for all Reviews
 
-const secondReview = new Review_Link(
-	"El Rey Mezcal Bar",
-	"This taco place has a squid ink taco that will blow your socks off",
-	"#",
-	"../images/elrey.jpeg",
-	"July 21, 2021"
-)
+const archiveArray = [
+	new Review_Link(
+		"Grand Electric",
+		"This Parkdale based taco joint covers all the bases, great tacos, great margs, great atmosphere",
+		`#`,
+		"../images/grandelectric.jpeg",
+		"July 28, 2021"
+	),
+	new Review_Link(
+		"El Rey Mezcal Bar",
+		"This taco place has a squid ink taco that will blow your socks off",
+		"#",
+		"../images/elrey.jpeg",
+		"July 21, 2021"
+	),
+	new Review_Link(
+		"Seven Lives",
+		"Don't let the queue put you off, you need to try these take out tacos",
+		"#",
+		"../images/sevenlives.jpeg",
+		"July 14, 2021"
+	),
+	new Review_Link(
+		"Good Hombres",
+		"Make your taco sets delivered to your door!",
+		"#",
+		"../images/goodhombres.jpeg",
+		"July 07, 2021"
+	),
+	new Review_Link(
+		"Mi Taco Taqueria",
+		"This Parkdale based taco joint covers all the bases, great tacos, great margs, great atmosphere",
+		`#`,
+		"../images/grandelectric.jpeg",
+		"June 30, 2021"
+	),
+	new Review_Link(
+		"Playa Cabana",
+		"This taco place has a squid ink taco that will blow your socks off",
+		"#",
+		"../images/elrey.jpeg",
+		"June 23, 2021"
+	),
+	new Review_Link(
+		"El Nahual Tacos",
+		"Don't let the queue put you off, you need to try these take out tacos",
+		"#",
+		"../images/sevenlives.jpeg",
+		"June 16, 2021"
+	),
+	new Review_Link(
+		"Campechano Taquería",
+		"Make your taco sets delivered to your door!",
+		"#",
+		"../images/goodhombres.jpeg",
+		"June 09, 2021"
+	),
+	new Review_Link(
+		"La Carnita",
+		"This Parkdale based taco joint covers all the bases, great tacos, great margs, great atmosphere",
+		`#`,
+		"../images/grandelectric.jpeg",
+		"June 02, 2021"
+	),
+	new Review_Link(
+		"Wilbur Mexicana",
+		"This taco place has a squid ink taco that will blow your socks off",
+		"#",
+		"../images/elrey.jpeg",
+		"May 26, 2021"
+	),
+	new Review_Link(
+		"Tacos El Asador",
+		"Don't let the queue put you off, you need to try these take out tacos",
+		"#",
+		"../images/sevenlives.jpeg",
+		"May 19, 2021"
+	),
+	new Review_Link(
+		"El Catrin Destileria",
+		"Make your taco sets delivered to your door!",
+		"#",
+		"../images/goodhombres.jpeg",
+		"May 12, 2021"
+	)
+];
 
-const thirdReview = new Review_Link(
-	"Seven Lives",
-	"Don't let the queue put you off, you need to try these take out tacos",
-	"#",
-	"../images/sevenlives.jpeg",
-	"July 14, 2021"
-)
+// Second is the main array for the most recent reviews showing on the home page
 
-const fourthReview = new Review_Link(
-	"Good Hombres",
-	"Make your taco sets delivered to your door!",
-	"#",
-	"../images/goodhombres.jpeg",
-	"July 07, 2021"
-)
+const mainArray = [];
 
-const mainReviewArray = [firstReview, secondReview, thirdReview, fourthReview];
+for (let i = 0; i < 4; i++) {
+	mainArray[i] = archiveArray[i]
+};
 
-export default mainReviewArray;
+// Export both arrays
+
+export {archiveArray, mainArray};
--- script/script.js
+++ script/script.js
@@ -1,84 +1,94 @@
-import mainReviewArray from "./data.js";
+import {archiveArray, mainArray} from "./data.js";
 
-// Create Reviews
-
+// Declare Variables
+const header = document.querySelector("header");
+const headNav = document.querySelector(".headNav");
+const headNavList = document.querySelector(".headNavList");
 const mainSection = document.querySelector(".mainSection");
-
-mainReviewArray.forEach((item) => {
-	const reviewItem = document.createElement("a");
-	reviewItem.classList.add("reviewLink");
-	reviewItem.setAttribute("href", `${item.link}`);
-	reviewItem.setAttribute("tabindex", "0");
-	reviewItem.innerHTML = `
-			<article class="review" style="background-image:url(${item.image})">
-				<h4 class="reviewDate">${item.date}</h4>
-				<span class="reviewHeadings">
-					<h2 class="headline">${item.headline}</h2>
-					<h3 class="subhead">${item.subhead}</h3>
-				</span>
-			</article>
-	`
-	mainSection.append(reviewItem);
-});
-
-// document.querySelectorAll(".reviewItem").forEach((item) => {
-// 	addEventListener(
-//   "mouseenter",
-//   () => {
-//     reviewItem.classList.add("blue");
-//   },
-//   false
-// );
-// });
-//
-// document.querySelectorAll(".reviewItem").forEach((item) => {
-// 	addEventListener(
-//   "mouseleave",
-//   () => {
-//     reviewItem.classList.add("blue");
-//   },
-//   false
-// );
-// });
-
-// Overlay Function
-
-// Create hamburger / x icons
-
-// Hamburger
+const homeLink = document.querySelector(".home");
+const archiveLink = document.querySelector(".archive");
+const newButton = document.createElement("button");
 const hbBtn = `
 	<svg class="headMenu" xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 24 24" width="3em" fill="#ED1D20">
 		<path d="M0 0h24v24H0V0z" fill="none"/>
 		<path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
 	</svg>
 `;
-
-// X
 const xBtn = `
 	<svg class="headMenu" xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 24 24" width="3em" fill="#ED1D20">
 		<path d="M0 0h24v24H0V0z" fill="none"/>
 		<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/>
 	</svg>
 `
+let windowWidth;
 
-// Turn icons into buttons
+// Declare Functions
+// Populate reviews from the arrays
+function sectionRefresh(array) {
+	mainSection.innerHTML = ``;
+	array.forEach((item) => {
+		const reviewItem = document.createElement("a");
+		reviewItem.classList.add("reviewLink");
+		reviewItem.setAttribute("href", `${item.link}`);
+		reviewItem.innerHTML = `
+				<article class="review" style="background-image:url(${item.image})">
+					<h4 class="reviewDate">${item.date}</h4>
+					<span class="reviewHeadings">
+						<h2 class="headline">${item.headline}</h2>
+						<h3 class="subhead">${item.subhead}</h3>
+					</span>
+				</article>
+		`
+		mainSection.append(reviewItem);
+	});
+};
 
-const newButton = document.createElement("button");
+// Reset mobile overlay
+function resetOverlay() {
+	header.removeAttribute('style');
+	headNav.removeAttribute('style');
+	headNavList.removeAttribute('style');
+	if (btnStatus !== true){
+		newButton.innerHTML = hbBtn;
+		btnStatus = true;
+	};
+};
+
+// Loads the homepage version of the array on page load. Will need to change/work on this
+window.addEventListener('load', (event) => {
+	sectionRefresh(mainArray);
+});
+
+// Changes content to the archive content when archive button is pressed
+archiveLink.addEventListener('click', () => {
+	mainSection.className = "archiveSection";
+	sectionRefresh(archiveArray);
+	archiveLink.style.display = "none";
+	homeLink.style.display = "block";
+	resetOverlay();
+});
+
+// Changes content to the homepage content when home button is pressed
+homeLink.addEventListener('click', () => {
+	mainSection.className = "mainSection";
+	sectionRefresh(mainArray);
+	homeLink.style.display = "none";
+	archiveLink.style.display = "block";
+	resetOverlay();
+});
+
+// Turn hamburger/X icons into a button and append it to the header
 newButton.classList.add("openBtn");
 newButton.innerHTML = hbBtn;
 let btnStatus = true;
+header.append(newButton);
 
 // Function to open and close the overlay
-
-const header = document.querySelector("header");
-const headNav = document.querySelector(".headNav");
-const headNavList = document.querySelector(".headNavList");
-
 newButton.addEventListener('click', () => {
 	if (btnStatus === true) {
 		btnStatus = false;
 		header.style.height = "100vh";
-		headNav.style.display = "block";
+		headNav.style.display = "flex";
 		header.addEventListener('transitionend', () => {
 			headNavList.style.opacity = "1";
 		});
@@ -94,25 +104,31 @@ newButton.addEventListener('click', () => {
 	}
 });
 
-// Reset everything on display change
-
-let windowWidth;
-
+// Reset everything on change from mobile view to tablet/desktop views
 setInterval(() => {
 	windowWidth = window.innerWidth;
 	if (windowWidth > 425) {
-		// document.querySelector("header").style.removeProperty("height");
-		// document.querySelector(".headNav").style.removeProperty("display");
-		header.removeAttribute('style');
-		headNav.removeAttribute('style');
-		headNavList.removeAttribute('style');
-		if (btnStatus !== true){
-			newButton.innerHTML = hbBtn;
-			btnStatus = true;
-		}
+	resetOverlay();
 	};
-}, 1);
+}, 300);
 
-// Append button to header
-
-header.append(newButton);
+// This is an idea I was testing but have left behind for now
+// document.querySelectorAll(".reviewItem").forEach((item) => {
+// 	addEventListener(
+//   "mouseenter",
+//   () => {
+//     reviewItem.classList.add("test");
+//   },
+//   false
+// );
+// });
+//
+// document.querySelectorAll(".reviewItem").forEach((item) => {
+// 	addEventListener(
+//   "mouseleave",
+//   () => {
+//     reviewItem.classList.remove("test");
+//   },
+//   false
+// );
+// });
